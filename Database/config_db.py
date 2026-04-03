9400cbb28f3a4d45a7d6649b15b03957e0b946d6
aliranjan206-stack <aliranjan206@gmail.com>
2026-04-03 22:37:42 +0530

    Update config_db.py

--- Database/config_db.py
+++ Database/config_db.py
@@ -1,4 +1,7 @@
 async def delete_all_messages(self):
 await self.col.delete_many(())
-mdb = Database(DATABASE_URI, "admin_database")
-ndb = Database(DATABASE_ URI, "adnin_ database")
+- 
+- mdb = Database(DATABASE_URI, "admin_database")
++
++ mdb = Database(DATABASE_ URI, "adnin_ database")
+
